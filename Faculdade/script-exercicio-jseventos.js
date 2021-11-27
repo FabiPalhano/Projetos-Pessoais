21680076129a179f5f8c3437a4491bc2951d800f
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2021-11-27 19:18:18 -0300

    atualizar

---
+++ Faculdade/script-exercicio-jseventos.js
@@ -0,0 +1,12 @@
+<script>
+    
+</script>
+
+
+//if = se
+//else = senão
+//else if = senão se
+//== comparação
+//= atribuição
+//escrevendo na tela = document.write()
+//concatenação = juntar palavras com variáveis = ("palavra" + variavel)
